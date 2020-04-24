b232833dbde02a54ef71087e51106ae93ba60768
vladimir <vladimir.fodorow@gmail.com>
2020-04-25 01:59:31 +0700

    add main

--- main.lua
+++ main.lua
@@ -4,4 +4,10 @@
 --
 -----------------------------------------------------------------------------------------
 
--- Your code here
+local composer = require("composer")
+
+-- Hide status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- Go to the menu screen
+composer.gotoScene("menu")
